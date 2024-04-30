f9bab9206d47b9e05c14fbb4fbacb0c4ecbebd65
Artur Nikytenko <arturnikytenko@gmail.com>
2024-04-30 10:07:50 +0200

    Tabbed component

--- script.js
+++ script.js
@@ -40,11 +40,29 @@ btnScrollTo.addEventListener('click', function (e) {
   });
 });
 
-document.querySelectorAll('.nav__link').addEventListener('click', function (e) {
+document.querySelector('.nav__link').addEventListener('click', function (e) {
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({
       behavior: 'smooth'
-    })
+    });
   }
-})
+});
+
+//Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
